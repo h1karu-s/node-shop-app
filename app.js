39935df27a134f96bdc0775551cff16043d0ec3d
hikaru-sh <polaris.2.71828@gmail.com>
2020-07-31 13:04:54 +0900

    add ejs

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const {shopRoutes} = require('./routes/shop');
 const {adminRoutes} = require('./routes/admin');
@@ -9,13 +8,8 @@ const {adminRoutes} = require('./routes/admin');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-app.engine('hbs',expressHbs({
-  layoutsDir:'views/layouts/',
-  defaultLayout:'main-layout',
-  extname:'hbs'
-}));
 
-app.set('view engine','hbs');
+app.set('view engine','ejs');
 app.set('views','views');
 
 app.use(bodyParser.urlencoded({extended:false}));
@@ -27,7 +21,8 @@ app.use(shopRoutes);
 app.use((req,res,next) => {
   // res.status(404).sendFile(path.join(__dirname,'views/404.html'))
   res.status(404).render('404',{
-    pageTitle:'Page Not Found!'
+    pageTitle:'Page Not Found!',
+    path:'not found'
   });
 })
 
